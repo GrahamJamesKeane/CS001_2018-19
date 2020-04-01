8675bfaf94eea826a5dc5b87d00d70c0e817af18
Graham Keane <40239414+GrahamJamesKeane@users.noreply.github.com>
2020-04-01 21:08:26 +0100

    Add files via upload

---
+++ L1Q4_18146619.py
@@ -0,0 +1,16 @@
+# Graham Keane 18146619 CS001
+# Question 4
+
+# Create variables X and Y:
+
+x = input("ENTER A NUMBER, X, PLEASE: ")
+
+y = input("ENTER ANOTHER NUMBER, Y, PLEASE: ")
+
+# Print the average, sum, and product of the variables x and y:
+
+print("THE AVERAGE OF X AND Y IS: " + str((float(x) + float(y)) / 2) + str("."))
+
+print("THE SUM OF X AND Y IS: " + str(float(x) + float(y)) + str("."))
+
+print("THE PRODUCT OF X AND Y IS: " + str(float(x) * float(y)) + str("."))
